3820e17248049986863b8b3c545ba766df1915fa
13alvone <13alvone@gmail.com>
2020-12-26 00:30:32 -0600

    ** Updated components with functions to make the response more durable ** Added reporting components and a self-referencing iterator to process certain tasks ** This is still under construction.

--- iphone_backup_parser.py
+++ iphone_backup_parser.py
@@ -1,10 +1,12 @@
 import subprocess
 import argparse
 import sqlite3
+import re
 
 # Global Variables
 data_set = set()
 report_list = []
+url_set = set()
 
 
 def is_sqlite3(_path):
@@ -43,20 +45,45 @@ def convert_sqlite3_to_sql_dict(_path):
 
 
 def iterate_sql_dict(_sqlite3_dict):
-    global data_set, report_list
+    global data_set, report_list, url_set
     for key in _sqlite3_dict:
         report_list.append(f'[+] Item: {key}\n')
+        re_extract_urls(key)
         if isinstance(_sqlite3_dict[key], dict):
             iterate_sql_dict(_sqlite3_dict[key])
         else:
             data_body = _sqlite3_dict[key]
             report_list.append(f'[^] {data_body}\n')
-            ## DO THINGS HERE FOR ITERATOR. DATA AT THIS POINT IS A TUPLE
-            # OLD: data_set.add(data_body[1])
-            for item in data_body:
-                if isinstance(item, bytes):
-                    ascii_data = ascii(item)
-                    print(ascii_data)
+            # DO THINGS HERE FOR ITERATOR. DATA AT THIS POINT IS A TUPLE
+            for data_item in data_body:
+                re_extract_urls(data_item)
+
+
+def re_extract_urls(_data):
+    global url_set
+    if isinstance(_data, list) or isinstance(_data, tuple):
+        for list_entry in _data:
+            url_list = re.findall(r'(https?://\S+)', list_entry)
+            for url in url_list:
+                url_set.add(url)
+    elif isinstance(_data, dict):
+        for key in _data:
+            url_list0 = re.findall(r'(https?://\S+)', key)
+            url_list1 = re.findall(r'(https?://\S+)', _data[key])
+            for url in url_list0:
+                url_set.add(url)
+            for url in url_list1:
+                url_set.add(url)
+    elif isinstance(_data, str):
+        url_list = re.findall(r'(https?://\S+)', _data)
+        for url in url_list:
+            url_set.add(url)
+    elif isinstance(_data, bytes):
+        url_list = re.findall(r'(https?://\S+)', _data.decode('ascii', 'ignore'))
+        for url in url_list:
+            url_set.add(url)
+    else:
+        pass
 
 
 def process_data_set(_sqlite3_dict, print_output=False):
@@ -71,7 +98,7 @@ def process_data_set(_sqlite3_dict, print_output=False):
 
 
 def main():
-    global data_set
+    global data_set, url_set
     args = parse_args()
     file_path = args.file_path
     if is_sqlite3(file_path):
@@ -82,6 +109,8 @@ def main():
     else:
         print('test')
         print(f'[!] The following supplied file path is not SQLite3\n{file_path}\n')
+    for url in url_set:
+        print(f'\n{"-" * 30}\n{url}\n')
 
 
 if __name__ == "__main__":
